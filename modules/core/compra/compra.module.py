5ad163a1bc4d3bff8323e6032950f9e7b1e87383
hugobarrioser <hugobarrioser@gmail.com>
2020-05-29 13:52:39 -0600

    Add nueva compra fuincionando

--- modules/core/compra/compra.module.py
+++ modules/core/compra/compra.module.py
@@ -9,33 +9,36 @@ inputU = loader.load_module('inputU')
   Proovedores
 '''
 # Menu del modulo
-def moduleMenu(dbConection, db, titulo = '+++ Seleccione una opción del menú +++'):
+def moduleMenu(dbConection, db,titulo="+++ Selecciona una opcion +++"):
   # Usamos cls para borrar la terminal en windows o clear para linux
   os.system('cls' if os.name == 'nt' else 'clear')
-  print('Modulo de clientes', '\n')
-  print(titulo, '\n')
-  print('1. Mostrar clientes')
-  print('2. Agregar nuevo cliente')
-  print('3. Eliminar cliente')
-  print('4. Regresar al menu principal')
+  print('Modulo de Compras', '\n')
+  print(titulo)
+  print('1. Agregar compra')
+  print('2. Mostrar Proveedores')
+  print('3. Agregar nuevo Proveedor')
+  print('4. Eliminar Proveedor')
+  print('5. Regresar al menu principal')
   opcion = inputU.inputNumber("\n-> ")
   # Llamamos a la funcion correcta para cada opcion del menu
   if opcion == 1:
-    showClients(dbConection, db)
+    compra(dbConection, db)
   elif opcion == 2:
-    addClient(dbConection, db)
+    showProvs(dbConection, db)
   elif opcion == 3:
-    removeClient(dbConection, db)
+    addProv(dbConection, db)
   elif opcion == 4:
+    removeProv(dbConection, db)
+  elif opcion == 5:
     pass
   else:
     # Si no se ingreso un valor valido entonces regresamos al usuario al menu con un mensaje de error
     moduleMenu(dbConection, db, titulo = '+++ Error: Opción no valida\t\t+++\n+++ Seleccione una opción del menú\t+++')
-# Funcion para mostrar clientes
-def showClients(dbConection, db):
+# Funcion para mostrar Proveedores
+def showProvs(dbConection, db):
   os.system('cls' if os.name == 'nt' else 'clear')
   print('-> Mostrando clientes <-\n')
-  db.execute('SELECT * FROM clientes')
+  db.execute('SELECT * FROM proveedores')
   results = db.fetchall()
   if len(results) < 1:
     print('No hay registros en la base de datos')
@@ -44,22 +47,22 @@ def showClients(dbConection, db):
       print("Nombre: " + str(row[1]) + "\tNit: " + str(row[2]))
   input('\nPrecione enter para continuar...')
   moduleMenu(dbConection, db)
-# Funcion para agregar clientes
-def addClient(dbConection, db):
+# Funcion para agregar Proveedor
+def addProv(dbConection, db):
   os.system('cls' if os.name == 'nt' else 'clear')
-  print('-> Agregar un cliente <-\n')
+  print('-> Agregar un Proveedor <-\n')
   nombre = inputU.inputRequired('Nombre: ')
   nit = inputU.inputNumber('Nit: ')
-  db.execute('INSERT INTO clientes (nombre, nit) VALUES ("' + nombre + '", ' + str(nit)  + ')')
+  db.execute('INSERT INTO proveedores (nombre, nit) VALUES ("' + nombre + '", ' + str(nit)  + ')')
   dbConection.commit()
   input('\nPrecione enter para continuar...')
   moduleMenu(dbConection, db)
-# Funcion para borrar clientes
-def removeClient(dbConection, db):
+# Funcion para borrar Proveedores
+def removeProv(dbConection, db):
   os.system('cls' if os.name == 'nt' else 'clear')
-  print('-> Eliminar un cliente <-\n')
+  print('-> Eliminar un Proveedor <-\n')
   nit = inputU.inputNumber('Nit: ')
-  db.execute('DELETE FROM clientes WHERE nit=' + str(nit))
+  db.execute('DELETE FROM proveedores WHERE nit=' + str(nit))
   dbConection.commit()
   input('\nPrecione enter para continuar...')
   moduleMenu(dbConection, db)
@@ -67,12 +70,27 @@ def removeClient(dbConection, db):
 '''
   Compras
 '''
+#modulo para agregar nueva compra
+def compra(dbConection, db):
+  os.system('cls' if os.name == 'nt' else 'clear')
+  print('-> Agregar una compra <-\n')
+  nombreprod = inputU.inputRequired('Nombre del producto: ')
+  descripcionprod = inputU.inputRequired('Descripcion del producto: ')
+  precioprod = inputU.inputNumber('Precio del producto: ')
+  unidadesprod = inputU.inputNumber('Unidades adquiridas: ')
+  db.execute('INSERT INTO inventario (nombre_prod, desc_prod, precio_prod, stock) VALUES ("' + nombreprod + '", "'+descripcionprod + '",'  +  str(precioprod)  +','+  str(unidadesprod)  +')')
+  #db.execute('INSERT INTO invetario (nombre_prod, nit) VALUES ("' + nombre + '", ' + str(nit)  + ')')
+  dbConection.commit()
+  input('\nPrecione enter para continuar...')
+  moduleMenu(dbConection, db)
 def mainFunction(mainMenu):
   # Usamos cls para borrar la terminal en windows o clear para linux
   os.system('cls' if os.name == 'nt' else 'clear')
   # Nos conectamos a la base de datos y creamos un cursor
   dbConection = sqlite3.connect('./_database_/db.sql')
   db = dbConection.cursor()
+  #
+  moduleMenu(dbConection, db)
   # Nos desconectamos de la base de datos
   dbConection.close()
   # REGRESAL AL MENU PRINCIPAL
